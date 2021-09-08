3475c563e338118bd2ba2aaf199cee6405fdf522
Serhii Siuziev <snake_wizard@protonmail.com>
2021-09-08 11:10:12 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { APIService } from './API.service';
+import { Restaurant } from '../types/restaurant';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'amplify-app2';
+
+export class AppComponent implements OnInit {
+  title = 'amplify-angular-app';
+  // @ts-ignore
+  public createForm: FormGroup;
+
+  /* declare restaurants variable */
+  // @ts-ignore
+  restaurants: Array<Restaurant>;
+
+  constructor(private api: APIService, private fb: FormBuilder) { }
+
+  async ngOnInit() {
+    this.createForm = this.fb.group({
+      'name': ['', Validators.required],
+      'description': ['', Validators.required],
+      'city': ['', Validators.required]
+    });
+    this.api.ListRestaurants().then(event => {
+      // @ts-ignore
+      this.restaurants = event.items;
+    });
+
+    /* subscribe to new restaurants being created */
+    this.api.OnCreateRestaurantListener.subscribe( (event: any) => {
+      const newRestaurant = event.value.data.onCreateRestaurant;
+      this.restaurants = [newRestaurant, ...this.restaurants];
+    });
+  }
+
+  public onCreate(restaurant: Restaurant) {
+    this.api.CreateRestaurant(restaurant).then(event => {
+      console.log('item created!');
+      this.createForm.reset();
+    })
+      .catch(e => {
+        console.log('error creating restaurant...', e);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+
+/* new form imports */
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +14,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AmplifyUIAngularModule,
+    /* configuring form modules */
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
